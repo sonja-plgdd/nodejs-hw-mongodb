d310ce38f1317f9429337f041d61894c413db58e
sonja <signtieva@gmail.com>
2025-03-06 13:40:39 +0200

    new routes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import router from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
 
@@ -16,50 +18,15 @@ export const setupServer = () => {
       },
     }),
   );
-
   app.use(cors());
-  app.use(express.json());
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    try {
-      const contactId = req.params.contactId;
-      const contact = await getContactById(contactId);
-      if (!contact) {
-        res.json({
-          status: 404,
-          message: 'Contact not found',
-        });
-        return;
-      }
-
-      res.json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (err) {
-      res.status(400).json({ message: `Contact id does not exist` });
-      next(err);
-    }
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
+  app.use(router);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,33 @@ export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+  };
+};
